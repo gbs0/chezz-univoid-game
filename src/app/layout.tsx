ede97ce25e59f371618cc9a3af2deaa77e72bac1
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2025-05-09 13:56:25 -0300

    Feat: Fix SSR error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body suppressHydrationWarning={true}>
         {children}
       </body>
     </html>
